a75ec3dd3e69fce3f4ce48b83514e2f50a066210
elgizabbasov <lgzabbasov@gmail.com>
2023-04-30 20:17:26 -0600

    added azure AD hook client ID

--- app/src/main.ts
+++ app/src/main.ts
@@ -20,11 +20,11 @@ import Vuetify from 'vuetify/lib'
 
 Vue.config.productionTip = false;
 
-// Vue.use(msal, {
-//  auth: {
-//    clientId: ""
-//  }
-// });
+Vue.use(msal, {
+ auth: {
+   clientId: "3630589c-51e7-43d3-a19b-cf635b240b1e"
+ }
+});
 
 Vue.use(Vuex)
 Vue.component("sidebar-component", Sidebar)
